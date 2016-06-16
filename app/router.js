a66259f56d226b95da9e415010c98e2edc39223e
Edmundo Figueroa <figuedmundo@gmail.com>
2016-06-16 01:56:14 -0400

    added ember-paper

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('places');
+  this.route('ways', function() {
+    this.route('show', {
+      path: ':source/:target'
+    });
+  });
 });
 
 export default Router;
--- config/environment.js
+++ config/environment.js
@@ -16,15 +16,52 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+      
     }
+  };
+  
+
+
+// contentSecurityPolicy: {
+//         'default-src': "'none'",
+//         'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
+//         'font-src': "'self' https://fonts.gstatic.com",
+//         'connect-src': "'self'",
+//         'img-src': "'self'",
+//         'report-uri':"'localhost'",
+//         'style-src': "'self' 'unsafe-inline'",
+//         'frame-src': "'none'"
+//       }
+
+  // ENV.contentSecurityPolicy = {
+  //     'default-src': "'none'",
+  //     'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
+  //     'font-src': "'self' http://fonts.gstatic.com",
+  //     'connect-src': "'self'",
+  //     'img-src': "'self' data:",
+  //     'media-src': "'self'",
+  //     'style-src': "'self' 'unsafe-inline'",
+  //     'frame-src': "'none'"
+  //   };
+
+  ENV.contentSecurityPolicy = {
+    'default-src': "'none'",
+    'script-src': "'self' 'unsafe-inline'",
+    'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com",
+    'font-src': "'self' fonts.gstatic.com",
+    'connect-src': "'self'",
+    'img-src': "'self' data:",
+    'media-src': "'self'"
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    ENV.APP.LOG_RESOLVER = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    
   }
 
   if (environment === 'test') {
