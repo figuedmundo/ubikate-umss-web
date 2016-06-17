470191b2fd046d6891268df40e00736ccf057aeb
Edmundo Figueroa <figuedmundo@gmail.com>
2016-06-17 02:44:14 -0400

    Added ember-leaflet

--- app/router.js
+++ app/router.js
@@ -6,7 +6,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('places');
+  this.route('places', function() {
+    this.route('show', {
+      path: ':name'
+    });
+    
+  });
   this.route('ways', function() {
     this.route('show', {
       path: ':source/:target'
--- app/routes/places/show.js
+++ app/routes/places/show.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    // lat: 45.519743,
-    // lng: -122.680522,
-    // zoom: 10
 });
